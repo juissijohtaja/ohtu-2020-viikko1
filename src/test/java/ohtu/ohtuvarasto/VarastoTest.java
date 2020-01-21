a93bb625a98048ec88540e18c1566ab20029aca0
JuissiJohtaja <juha.koivu@outlook.com>
2020-01-21 17:12:11 +0200

    testi

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -73,7 +73,7 @@ public class VarastoTest {
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
-    // omat testit
+    // omat testit 2
 
     @Test
     public void lisaysTayteenMaxSaldo() {
