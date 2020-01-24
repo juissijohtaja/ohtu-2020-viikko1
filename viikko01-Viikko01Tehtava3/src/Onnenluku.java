43536731f69394aec1a0fad92405df060a557442
JuissiJohtaja <44608242+juissijohtaja@users.noreply.github.com>
2020-01-24 17:44:06 +0200

    Delete Onnenluku.java

--- viikko01-Viikko01Tehtava3/src/Onnenluku.java
+++
@@ -1,8 +0,0 @@
-public class Onnenluku {
-    public boolean tarkasta(int n) {
-        if (!String.valueOf(n).matches("[47]+")) {
-            return false;
-        }
-            return true;
-    }
-}
